17f44faf49d538621480f26754af22e55e00f111
baldore <acornssn@gmail.com>
2020-04-14 12:57:49 -0500

    Fix z command

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -2,5 +2,5 @@ source /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.c
 fpath+=( /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-wfxr-SLASH-forgit/forgit.plugin.zsh
 fpath+=( /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-wfxr-SLASH-forgit )
-source /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z/zsh-z.plugin.zsh
-fpath+=( /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z )
+source /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( /Users/aorrego/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
