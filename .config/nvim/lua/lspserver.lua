44b70819f772604f398541acca71f300f6325a3f
baldore <acornssn@gmail.com>
2020-09-09 15:43:21 -0500

    Extract lsp initialization in a different file

---
+++ .config/nvim/lua/lspserver.lua
@@ -0,0 +1,9 @@
+local lsp = require 'nvim_lsp'
+
+lsp.tsserver.setup{on_attach=require'diagnostic'.on_attach}
+lsp.gopls.setup{on_attach=require'diagnostic'.on_attach}
+lsp.vuels.setup{on_attach=require'diagnostic'.on_attach}
+lsp.sumneko_lua.setup{on_attach=require'diagnostic'.on_attach}
+lsp.vimls.setup{}
+lsp.cssls.setup{}
+
